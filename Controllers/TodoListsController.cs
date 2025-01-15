950acb2632f32f1331b1b7d3e0d0e02302eb973f
Gaetanerk <gaetan.turben@outlook.com>
2025-01-15 11:45:38 +0100

    filtres et annotations

--- Controllers/TodoListsController.cs
+++ Controllers/TodoListsController.cs
@@ -20,16 +20,32 @@ namespace TPTodoList.Controllers
         }
 
         // GET: TodoLists
-        public async Task<IActionResult> Index(int pageNumber = 1, int pageSize = 10)
+        public async Task<IActionResult> Index(string filter = "all", int pageNumber = 1, int pageSize = 10)
         {
-            var totalItems = await _context.TodoList.CountAsync();
-            var todoLists = await _context.TodoList
+            IQueryable<TodoList> query = _context.TodoList;
+
+            switch (filter.ToLower())
+            {
+                case "finished":
+                    query = query.Where(t => t.IsComplete);
+                    break;
+                case "inprogress":
+                    query = query.Where(t => !t.IsComplete);
+                    break;
+                default:
+                    break;
+            }
+
+            var totalItems = await query.CountAsync();
+            var todoLists = await query
+                .OrderByDescending(t => t.IsComplete)
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize)
                 .ToListAsync();
 
             ViewBag.CurrentPage = pageNumber;
             ViewBag.TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+            ViewBag.Filter = filter;
 
             return View(todoLists);
         }
@@ -45,7 +61,7 @@ namespace TPTodoList.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Form([Bind("Id,Task,IsComplete")] TodoList todoList)
+        public async Task<IActionResult> Form([Bind("Id,Task,DueDate,IsComplete")] TodoList todoList)
         {
             if (ModelState.IsValid)
             {
@@ -55,8 +71,6 @@ namespace TPTodoList.Controllers
             }
             return View(todoList);
         }
-
-
         public async Task<IActionResult> MarkComplete(int id)
         {
             var todoList = await _context.TodoList.FindAsync(id);
--- Models/TodoList.cs
+++ Models/TodoList.cs
@@ -1,9 +1,22 @@
-﻿namespace TPTodoList.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPTodoList.Models
 {
     public class TodoList
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Tâche")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="La tâche doit contenir entre 3 et 50 caractères")]
         public string Task { get; set; }
+
+        [Required]
+        [Display(Name = "Date de réalisation")]
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
         public bool IsComplete { get; set; } = false;
     }
 }
